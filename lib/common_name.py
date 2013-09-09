bc2d8e54697e728fc7d024d6cd23c65a86aaeca1
lhe <lhe@kaiserhl.com>
2013-09-08 22:06:27 -0500

    tmuxinator project
    - create tmuxinator project
    
    - installing step is ready to go (tested)

---
+++ lib/common_name.py
@@ -0,0 +1,25 @@
+#
+# value for these arguments are from vimperator.conf
+# make sure configuration names in vimperator.conf matches the value of those 
+# variables
+#
+
+SECT_COMMON = 'common'
+
+# '~/.vim'
+VIMRC_PATH = 'vimrc_path'
+VIMRC_CONF_DIR = 'vimrc_conf_dir'
+VIMRC_PLUGIN_DIR = 'vimrc_plugin_dir'
+
+
+# each section has different confs & plugins
+VIMRC_CONF = 'vimrc_conf'
+VIMRC_PLUGIN = 'vimrc_plugin'
+
+VIMERATOR_CONF_PATH = 'vimerator_conf_path'
+VIMERATOR_PLUGIN_PATH = 'vimerator_plugin_path'
+VIMERATOR_PRESET_PATH = 'vimerator_preset_path'
+
+
+
+
