820437398dda3f76260a479e0ec8273a9f0e5b9c
u9E9F <u9E9F@kaiserhl.com>
2018-07-19 20:08:18 +0000

    Updated the lint/cc/main0.cc

--- test/lint/cc/main0.cc
+++ test/lint/cc/main0.cc
@@ -3,7 +3,12 @@
 #include <string>
 
 class Foo {
-  Foo(int f);
+  Foo(int f) {
+    var x = 3;
+    if true {
+      std::cout << "world" << std::endl;
+    }
+  }
 };
 
 int main(int argc, char *argv[]) {
