2fd20d39113da0ad3d38e2b200c7b2b90129660f
u9E9F <u9E9F@kaiserhl.com>
2019-04-23 22:45:20 -0500

    Update the vendor/vim/install.sh.

--- vendor/vim/install.sh
+++ vendor/vim/install.sh
@@ -1,5 +1,9 @@
 
+git clone https://github.com/vim/vim.git
 
 yum install ncurses-devel
+
+apt install build-essential
+apt-get install libncurses5-dev libncursesw5-dev
 
 ./configure --enable-python3interp=yes --with-python3-command=/usr/bin/python3
